12ee3ba43b58784863729c68a448939c17da9fe0
Jake <weednj@gmail.com>
2025-09-04 15:40:07 -0600

    Cool

--- app/AppState.js
+++ app/AppState.js
@@ -1,9 +1,15 @@
+
+import { Identity } from './Auth/Identity.js'
 import { gift } from './models/Gift.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 
 class ObservableAppState extends EventEmitter {
+
+  /** @type {Identity} */
+
+  identity = null
 
   /**@type {import('./models/Example.js').Example[]} */
   examples = []
